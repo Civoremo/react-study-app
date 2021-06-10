375d5a6a8603668abdbb8d6af8dd6de6324aa497
Civoremo <civoremon@gmail.com>
2021-06-10 19:10:26 -0400

    knexfile update

--- knexfile.js
+++ knexfile.js
@@ -36,18 +36,17 @@ module.exports = {
 	// },
 
 	production: {
-		client: 'postgresql',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password'
-		},
-		pool: {
-			min: 2,
-			max: 10
-		},
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		// pool: {
+		// 	min: 2,
+		// 	max: 10
+		// },
 		migrations: {
-			tableName: 'knex_migrations'
-		}
+			// tableName: 'knex_migrations'
+			directory: './db/migrations',
+			tableName: 'dbmigrations'
+		},
+		seeds: { directory: './db/seeds' }
 	}
 };
