6079d34cd1e3565ad27e350f2a6d1ed8475b8912
Civoremo <civoremon@gmail.com>
2021-06-10 18:50:05 -0400

    uncommented knexfile production settings

--- knexfile.js
+++ knexfile.js
@@ -4,19 +4,19 @@ module.exports = {
 	development: {
 		client: 'sqlite3',
 		connection: {
-			filename: './db/db.sqlite3',
+			filename: './db/db.sqlite3'
 		},
 		pool: {
 			afterCreate: function(conn, cb) {
 				conn.run('PRAGMA foreign_keys = ON', cb);
-			},
+			}
 		},
 		useNullAsDefault: true,
 		migrations: {
 			directory: './db/migrations',
-			tableName: 'dbmigrations',
+			tableName: 'dbmigrations'
 		},
-		seeds: { directory: './db/seeds' },
+		seeds: { directory: './db/seeds' }
 	},
 
 	// staging: {
@@ -35,19 +35,19 @@ module.exports = {
 	//   }
 	// },
 
-	// production: {
-	//   client: 'postgresql',
-	//   connection: {
-	//     database: 'my_db',
-	//     user:     'username',
-	//     password: 'password'
-	//   },
-	//   pool: {
-	//     min: 2,
-	//     max: 10
-	//   },
-	//   migrations: {
-	//     tableName: 'knex_migrations'
-	//   }
-	// }
+	production: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password'
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	}
 };
